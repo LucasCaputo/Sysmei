e4c25ad7a44da1f189f5278b57f0c192c35cae93
Lucas Caputo <lccaputo@hotmail.com>
2024-05-25 10:13:01 -0300

    feat: add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ` <app-loader></app-loader> <router-outlet></router-outlet>`,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
