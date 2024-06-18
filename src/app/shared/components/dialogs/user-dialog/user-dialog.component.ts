bd9b7c4ecd48f49391142d528512dfb3d3ec6093
LucasCaputo <lccaputo@hotmail.com>
2024-06-18 01:22:48 -0300

    feat: add user settingsdialog

---
+++ src/app/shared/components/dialogs/user-dialog/user-dialog.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@Component({
+  selector: 'app-user-dialog',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './user-dialog.component.html',
+  styleUrls: ['./user-dialog.component.scss']
+})
+export class UserDialogComponent {
+  private user = this.authService.getUser()
+
+  public userForm = this.formBuilder.group({
+    name: this.user?.nome || '',
+    phone: this.user?.telefone || '',
+    login: this.user?.login || ''
+  })
+
+  public passwordForm = this.formBuilder.group({
+    oldPassword: '',
+    password: '',
+    repetPassword: ''
+  })
+
+  public removeAccountForm = this.formBuilder.group({
+    enabled: true,
+    status: true,
+  })
+
+  public constructor(private authService: AuthService, private formBuilder: FormBuilder){
+    
+  }
+
+}
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
 import { CustomerService } from '../../services/customer/customer.service';
+import { UserDialogComponent } from '../dialogs/user-dialog/user-dialog.component';
 
 @Component({
   selector: 'app-menu',
@@ -38,7 +40,7 @@ export class MenuComponent {
     }
   ]
 
-  constructor(public auth: AuthService, public router: Router, private customerService: CustomerService) { }
+  constructor(public auth: AuthService, public router: Router, private customerService: CustomerService, private dialog: MatDialog) { }
 
   /** Faz logout do sistema */
   public logout(): void {
@@ -53,5 +55,23 @@ export class MenuComponent {
   /** Redireciona para rota selecionada */
   public redirect(router: string): void {
     this.router.navigate([router]);
+  }
+
+  public openUserSettingsModal(): void {
+    const dialogRef = this.dialog.open(UserDialogComponent, {
+      width: '500px',
+      maxWidth: '100vw',
+      position: {
+        top: '70px'
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (!result) {
+        return;
+      }
+
+      
+    });
   }
 }
