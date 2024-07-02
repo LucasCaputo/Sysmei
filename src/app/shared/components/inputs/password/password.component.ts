0453a98139ac4b935cf05873e8dfb73d12759454
LucasCaputo <lccaputo@hotmail.com>
2024-07-01 22:20:16 -0300

    feat: add password component

---
+++ src/app/shared/components/inputs/password/password.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@Component({
+  selector: 'app-password',
+  standalone: true,
+  imports: [SharedModule],
+  templateUrl: './password.component.html',
+  styleUrls: ['./password.component.scss']
+})
+export class PasswordComponent {
+  @Input({ required: true }) password!: FormControl<string | null>;
+  @Input({ required: true }) label!: string;
+  @Input() required = true;
+
+  type = 'password';
+}
--- src/app/shared/components/inputs/shared-input.module.ts
+++ src/app/shared/components/inputs/shared-input.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { NameComponent } from './name/name.component';
 import { PhoneComponent } from './phone/phone.component';
 import { EmailComponent } from './email/email.component';
+import { PasswordComponent } from './password/password.component';
 
 @NgModule({
   imports: [
     NameComponent,
     PhoneComponent,
-    EmailComponent
+    EmailComponent,
+    PasswordComponent
   ],
   exports: [
     NameComponent,
     PhoneComponent,
-    EmailComponent
+    EmailComponent,
+    PasswordComponent
   ]
 })
 export class SharedInputModule { }
