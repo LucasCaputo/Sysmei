cd47ff3033efe053871a7fb218d417c3a18d0336
LucasCaputo <lccaputo@hotmail.com>
2024-07-11 18:10:18 -0300

    refactor: page 404

--- src/app/pages/erro404/erro404.component.ts
+++ src/app/pages/erro404/erro404.component.ts
@@ -10,29 +10,16 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [SharedModule]
 })
 export class Erro404Component implements OnInit {
-  stars1: any[] = [];
-  stars2: any[] = [];
   birds: any[] = [];
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.generateStars();
     this.generateBirds();
   }
 
-  generateStars() {
-    for (let i = 1; i <= 30; i++) {
-      this.stars1.push({ id: i });
-    }
-
-    for (let i = 1; i <= 30; i++) {
-      this.stars2.push({ id: i });
-    }
-  }
-
   generateBirds() {
-    for (let i = 1; i <= 6; i++) {
+    for (let i = 1; i <= 3; i++) {
       this.birds.push({ id: i });
     }
   }
