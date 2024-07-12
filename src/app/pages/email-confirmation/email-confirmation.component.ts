6ab8530f29f90d3e712b0b2b3b03344744ebc179
francisco moreira <franciscon238@gmail.com>
2024-07-11 23:22:58 -0300

    feat: Add email confirmation page

--- src/app/pages/email-confirmation/email-confirmation.component.ts
+++ src/app/pages/email-confirmation/email-confirmation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @Component({
@@ -9,5 +10,11 @@ import { SharedModule } from 'src/app/shared/shared.module';
   styleUrls: ['./email-confirmation.component.scss']
 })
 export class EmailConfirmationComponent {
+
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 
 }
